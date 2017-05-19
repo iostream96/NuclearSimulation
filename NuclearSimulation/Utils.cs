5e9c9368be48f6b9b9f0f9d4e9515372dff5cd3e
Drupping <1628513611@qq.com>
2017-05-19 16:14:23 +0800

    test

--- NuclearSimulation/Utils.cs
+++ NuclearSimulation/Utils.cs
@@ -9,6 +9,7 @@ namespace NuclearSimulation
 {
     class Utils
     {
+        //calpoint
         public static List<Point> getPoint(int n, bool triggerFlag)
         {
             Random ran = new Random();
